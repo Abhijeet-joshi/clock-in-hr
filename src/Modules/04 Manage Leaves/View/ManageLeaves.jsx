cafea0319b4774108d9b68233efe50063e3f277f
Abhijeet-joshi <your.email@example.com>
2025-07-25 12:56:08 +0530

    created all landing page screens

--- src/Modules/04 Manage Leaves/View/ManageLeaves.jsx
+++ src/Modules/04 Manage Leaves/View/ManageLeaves.jsx
@@ -1,9 +1,59 @@
+import '../../../Common Utilities/tableLayout.css';
+
 export default function Page4(){
+
+    var allRequests = [
+        {
+            lid : '101',
+            eid : '1001',
+            type : 'CL',
+            off : 'Full Day',
+            date : '25/07/2025',
+            reason : 'Medical Issue'
+        },
+        {
+            lid : '102',
+            eid : '1002',
+            type : 'PL',
+            off : 'Half Day',
+            date : '25/07/2025',
+            reason : 'Health Checkup'
+        },
+    ];
+
     return (
+
         <div>
-            <center>
-                This is Page 4
-            </center>
-        </div>
+            <div className='searchFieldDiv'>
+            <input placeholder='Enter Employee ID'></input>
+            <div className='button'>Search</div>
+            </div>
+        
+
+        <table className="tableStyle">
+                <thead className='tableHead'>
+                    <tr>
+                        <td className='headData'>Leave ID</td>
+                        <td className='headData'>Employee ID</td>
+                        <td className='headData'>Leave Type</td>
+                        <td className='headData'>Leave For</td>
+                        <td className='headData'>Date</td>
+                        <td className='headData'>Reason</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {allRequests.map((req)=>{
+                        return <tr>
+                        <td className='empData'>{req.lid}</td>
+                        <td className='empData'>{req.eid}</td>
+                        <td className='empData'>{req.type}</td>
+                        <td className='empData'>{req.off}</td>
+                        <td className='empData'>{req.date}</td>
+                        <td className='empData'>{req.reason}</td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+            </div>
     );
 }
--- src/Modules/05 Leave Requests/View/LeaveRequests.jsx
+++ src/Modules/05 Leave Requests/View/LeaveRequests.jsx
@@ -1,9 +1,63 @@
+import '../../../Common Utilities/tableLayout.css';
+import '../Style/LeaveRequests.css';
+
 export default function Page5(){
+    var allRequests = [
+        {
+            lid : '101',
+            eid : '1001',
+            type : 'CL',
+            off : 'Full Day',
+            reason : 'Medical Issue',
+            date : '25/07/2025',
+            status : 'Pending',
+        },
+        {
+            lid : '102',
+            eid : '1002',
+            type : 'PL',
+            off : 'Half Day',
+            reason : 'Health Checkup',
+            date : '25/07/2025',
+            status : 'Approved',
+        },
+    ];
+
     return (
+
         <div>
-            <center>
-                This is Page 5
-            </center>
-        </div>
+            <div className='searchFieldDiv'>
+            <input placeholder='Enter Employee ID'></input>
+            <div className='button'>Search</div>
+            </div>
+        
+
+        <table className="tableStyle">
+                <thead className='tableHead'>
+                    <tr>
+                        <td className='headData'>Leave ID</td>
+                        <td className='headData'>Employee ID</td>
+                        <td className='headData'>Leave Type</td>
+                        <td className='headData'>Leave For</td>
+                        <td className='headData'>Reason</td>
+                        <td className='headData'>Date</td>
+                        <td className='headData'>Status</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {allRequests.map((req)=>{
+                        return <tr>
+                        <td className='empData'>{req.lid}</td>
+                        <td className='empData'>{req.eid}</td>
+                        <td className='empData'>{req.type}</td>
+                        <td className='empData'>{req.off}</td>
+                        <td className='empData'>{req.date}</td>
+                        <td className='empData'>{req.reason}</td>
+                        <td className={`${req.status == 'Pending' ? 'pending' : 'approved'}`}>{req.status}</td>
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+            </div>
     );
 }
